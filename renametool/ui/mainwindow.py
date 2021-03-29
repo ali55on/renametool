d8afba4adcfc280a572bb86a5626edd2c1a74b14
w-a-gomes <wildenalisson@outlook.com>
2021-03-29 06:20:24 -0300

    glib

--- renametool/ui/mainwindow.py
+++ renametool/ui/mainwindow.py
@@ -5,11 +5,13 @@ import os
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from gi.repository import GLib
 
 import tools.title as title
 import tools.utils.file as file
 import tools.settings as settings
 import ui.header as header
+import ui.selectfiles as select_files
 import ui.preview as preview
 import ui.base as base
 
@@ -30,10 +32,14 @@ class RenameToolWindow(Gtk.Window):
 
         # Flags
         self.status_error = None
+        self.files_preview = False
 
         # Window title
-        title_obj = title.Title(self.list_files)
-        self.set_title(title_obj.get_title())
+        if self.list_files:
+            title_obj = title.Title(self.list_files)
+            self.set_title(title_obj.get_title())
+        else:
+            self.set_title('Rename Tool')
 
         # Main box
         self.main_box = Gtk.VBox()
@@ -43,14 +49,35 @@ class RenameToolWindow(Gtk.Window):
         self.header = header.StackHeader(markup_settings=self.markup_settings)
         self.main_box.pack_start(self.header, True, True, 0)
 
-        # Preview
         sep_up = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL, valign=Gtk.Align.END)
         self.main_box.pack_start(sep_up, True, True, 0)
 
+        # Create Stack
+        self.stack = Gtk.Stack()
+        self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_UP_DOWN)
+        self.stack.set_transition_duration(300)
+
+        # Preview
         self.preview = preview.Preview(
-            header=self.header, color_settings=self.color_settings, markup_settings=self.markup_settings,
+            header=self.header, color_settings=self.color_settings,
+            markup_settings=self.markup_settings,
             list_files=self.list_files)
-        self.main_box.pack_start(self.preview, True, True, 0)
+
+        # Select
+        self.select_area = select_files.SelectFiles(list_files=self.list_files)
+
+        # Set stack
+        # self.active_work_tab = self.stack_switcher.get_stack().get_visible_child_name()
+        # self.stack.set_visible_child(self.preview)
+        # self.stack.set_visible_child(self.select_area)
+        if self.list_files:
+            self.stack.add_titled(self.preview, 'preview', 'preview')
+            self.stack.add_titled(self.select_area, 'select', 'select')
+        else:
+            self.stack.add_titled(self.select_area, 'select', 'select')
+            self.stack.add_titled(self.preview, 'preview', 'preview')
+
+        self.main_box.pack_start(self.stack, True, True, 0)
 
         # Base
         sep_dw = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL, valign=Gtk.Align.START)
@@ -68,6 +95,21 @@ class RenameToolWindow(Gtk.Window):
         # Default
         self.activate_default()
         self.set_default(self.base.button_rename)
+
+        self.preview_threading()
+
+    def preview_threading(self):
+        if not self.files_preview:
+            GLib.idle_add(self.preview_threading_glib)
+        GLib.timeout_add(300, self.preview_threading)
+
+    def preview_threading_glib(self):
+        self.list_files = self.select_area.get_file_list()
+        print(self.list_files )
+        if self.list_files:
+            self.files_preview = True
+            self.stack.set_visible_child(self.preview)
+            # self.preview.set_list()
 
 
 if __name__ == '__main__':
---
+++ renametool/ui/selectfiles.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class SelectFiles(Gtk.VBox):
+    """docstring for EmptyStartWindow"""
+    def __init__(self, list_files):
+        Gtk.VBox.__init__(self, height_request=300)
+
+        # Flag
+        self.file_list = list_files
+        self.can_update_list = False
+
+        # Choose Files Box
+        self.box_choose_files = Gtk.HBox(spacing=6, valign=Gtk.Align.CENTER, halign=Gtk.Align.CENTER)
+        self.pack_start(self.box_choose_files, True, True, 0)
+
+        # Choose Files Label
+        self.label_choose_files = Gtk.Label(label='Choose files to rename')
+        self.box_choose_files.pack_start(self.label_choose_files, True, True, 0)
+
+        # Choose Files Button
+        self.icon_open = Gtk.Image(icon_name='folder-open-symbolic')
+        self.button_choose_files = Gtk.Button(image=self.icon_open, always_show_image=True)
+        self.button_choose_files.connect('clicked', self.__on_select_files)
+        self.box_choose_files.pack_start(self.button_choose_files, True, True, 0)
+
+    def __on_select_files(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a file", 
+            action=Gtk.FileChooserAction.OPEN, select_multiple=True)
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK)
+
+        response = dialog.run()
+        
+        if response != Gtk.ResponseType.CANCEL:
+            self.file_list = dialog.get_uris()
+            # self.set_visible(False)
+
+        dialog.destroy()
+
+    def get_file_list(self):
+        return self.file_list
