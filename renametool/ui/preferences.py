c2a16b98a95c59f400ae8a46598944e9de0b922b
w-a-gomes <wildenalisson@outlook.com>
2021-04-07 13:49:06 -0300

    Preference - Replace text layout

--- renametool/ui/preferences.py
+++ renametool/ui/preferences.py
@@ -51,6 +51,9 @@ class AppPage(Gtk.VBox):
 
         :param app_settings: A 'dictionary' with app settings
         """
+        # warning and error messages color highlight 
+        # highlights warning color
+        # highlights error color
         Gtk.Box.__init__(self, margin=18, *args, **kwargs)
         self.pack_start(Gtk.Label(label='App Preferences'), True, True, 0)
 
@@ -70,70 +73,93 @@ class RenamePage(Gtk.VBox):
         # Settings
         self.markup_settings = settings.get_markup_settings()
 
-        self.main_box = Gtk.VBox(halign=Gtk.Align.START, valign=Gtk.Align.START)
+        self.main_box = Gtk.VBox(
+            spacing=6, halign=Gtk.Align.START, valign=Gtk.Align.START)
         self.pack_start(self.main_box, True, True, 0)
 
         # Automatic numbers title
         aut_num_title = Gtk.Label(
             label='Automatic numbers templates',
-            halign=Gtk.Align.START, margin_bottom=12)
+            halign=Gtk.Align.START, margin_bottom=6)
         self.main_box.pack_start(aut_num_title, True, True, 0)
 
-        # Automatic numbers containers
-        aut_num_main_box = Gtk.HBox(halign=Gtk.Align.END, spacing=6)
-        self.main_box.pack_start(aut_num_main_box, True, True, 0)
-
-        aut_num_labels = Gtk.VBox(spacing=6)
-        aut_num_main_box.pack_start(aut_num_labels, True, True, 0)
-
-        aut_num_values = Gtk.VBox(spacing=6)
-        aut_num_main_box.pack_start(aut_num_values, True, True, 0)
-
         # 1, 2, 3
-        label_1 = Gtk.Label('1, 2, 3 ', halign=Gtk.Align.END)
-        label_1.set_sensitive(False)
-        aut_num_labels.pack_start(label_1, True, True, 0)
+        aut_num_1_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
+        self.main_box.pack_start(aut_num_1_box, True, True, 0)
 
-        self.entry_1 = EntryMarkup(halign=Gtk.Align.END)
-        aut_num_values.pack_start(self.entry_1, True, True, 0)
+        label_1 = Gtk.Label('1, 2, 3 ')
+        label_1.set_sensitive(False)
+        aut_num_1_box.pack_start(label_1, True, True, 0)
+
+        label_1_open = Gtk.Label('[')
+        aut_num_1_box.pack_start(label_1_open, True, True, 0)
+
+        self.entry_1 = Gtk.Entry()
         self.entry_1.set_text(self.markup_settings['[1, 2, 3]'][1:-1])
+        aut_num_1_box.pack_start(self.entry_1, True, True, 0)
+
+        label_1_close = Gtk.Label(']')
+        aut_num_1_box.pack_start(label_1_close, True, True, 0)
 
         # 01, 02, 03
-        label_01 = Gtk.Label('01, 02, 03 ', halign=Gtk.Align.END)
-        label_01.set_sensitive(False)
-        aut_num_labels.pack_start(label_01, True, True, 0)
+        aut_num_01_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
+        self.main_box.pack_start(aut_num_01_box, True, True, 0)
 
-        self.entry_01 = EntryMarkup(halign=Gtk.Align.END)
-        aut_num_values.pack_start(self.entry_01, True, True, 0)
+        label_01 = Gtk.Label('01, 02, 03 ')
+        label_01.set_sensitive(False)
+        aut_num_01_box.pack_start(label_01, True, True, 0)
+
+        label_01_open = Gtk.Label('[')
+        aut_num_01_box.pack_start(label_01_open, True, True, 0)
+
+        self.entry_01 = Gtk.Entry()
         self.entry_01.set_text(self.markup_settings['[01, 02, 03]'][1:-1])
+        aut_num_01_box.pack_start(self.entry_01, True, True, 0)
+
+        label_01_close = Gtk.Label(']')
+        aut_num_01_box.pack_start(label_01_close, True, True, 0)
 
         # 001, 002, 003
-        label_001 = Gtk.Label('001, 002, 003 ', halign=Gtk.Align.END)
-        label_001.set_sensitive(False)
-        aut_num_labels.pack_start(label_001, True, True, 0)
+        aut_num_001_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
+        self.main_box.pack_start(aut_num_001_box, True, True, 0)
 
-        self.entry_001 = EntryMarkup(halign=Gtk.Align.END)
-        aut_num_values.pack_start(self.entry_001, True, True, 0)
+        label_001 = Gtk.Label('001, 002, 003 ')
+        label_001.set_sensitive(False)
+        aut_num_001_box.pack_start(label_001, True, True, 0)
+
+        label_001_open = Gtk.Label('[')
+        aut_num_001_box.pack_start(label_001_open, True, True, 0)
+
+        self.entry_001 = Gtk.Entry()
         self.entry_001.set_text(self.markup_settings['[001, 002, 003]'][1:-1])
+        aut_num_001_box.pack_start(self.entry_001, True, True, 0)
+
+        label_001_close = Gtk.Label(']')
+        aut_num_001_box.pack_start(label_001_close, True, True, 0)
 
         # Original filename
         orig_filename_title = Gtk.Label(
             label='Original filename template',
-            halign=Gtk.Align.START, margin_top=18, margin_bottom=12)
+            halign=Gtk.Align.START, margin_top=12, margin_bottom=6)
         self.main_box.pack_start(orig_filename_title, True, True, 0)
 
-        orig_filename_box = Gtk.HBox(halign=Gtk.Align.END, spacing=6)
+        orig_filename_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
         self.main_box.pack_start(orig_filename_box, True, True, 0)
 
-        label_orig_filename = Gtk.Label(
-            label='Original filename', halign=Gtk.Align.END)
+        label_orig_filename = Gtk.Label(label='Original filename')
         label_orig_filename.set_sensitive(False)
         orig_filename_box.pack_start(label_orig_filename, True, True, 0)
 
-        self.entry_filename = EntryMarkup(halign=Gtk.Align.END)
-        orig_filename_box.pack_start(self.entry_filename, True, True, 0)
+        label_orig_filename_open = Gtk.Label('[')
+        orig_filename_box.pack_start(label_orig_filename_open, True, True, 0)
+
+        self.entry_filename = Gtk.Entry()
         self.entry_filename.set_text(
             self.markup_settings['[original-name]'][1:-1])
+        orig_filename_box.pack_start(self.entry_filename, True, True, 0)
+
+        label_orig_filename_close = Gtk.Label(']')
+        orig_filename_box.pack_start(label_orig_filename_close, True, True, 0)
 
         # Base
         base_box = Gtk.HBox(
@@ -167,14 +193,41 @@ class ReplacePage(Gtk.VBox):
         self.color_settings = settings.get_color_settings()
 
         #
-        self.main_box = Gtk.VBox(halign=Gtk.Align.START, valign=Gtk.Align.START)
+        self.main_box = Gtk.VBox(
+            spacing=6, halign=Gtk.Align.START, valign=Gtk.Align.START)
         self.pack_start(self.main_box, True, True, 0)
 
-        # Automatic numbers title
-        aut_num_title = Gtk.Label(
-            label='Replace text',
+        # Colors title
+        colors_title = Gtk.Label(
+            label='Highlight colors of the preview',
             halign=Gtk.Align.START, margin_bottom=12)
-        self.main_box.pack_start(aut_num_title, True, True, 0)
+        self.main_box.pack_start(colors_title, True, True, 0)
+
+        # Search color
+        color_search_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
+        self.main_box.pack_start(color_search_box, True, True, 0)
+
+        label_search_color = Gtk.Label('Search color')
+        label_search_color.set_sensitive(False)
+        color_search_box.pack_start(label_search_color, True, True, 0)
+
+        self.entry_search_color = Gtk.Entry()
+        self.entry_search_color.set_text(
+            self.color_settings['old-matching-color'])
+        color_search_box.pack_start(self.entry_search_color, True, True, 0)
+
+        # Match color 
+        color_match_box = Gtk.HBox(spacing=6, halign=Gtk.Align.END)
+        self.main_box.pack_start(color_match_box, True, True, 0)
+
+        label_match_color  = Gtk.Label('Match color')
+        label_match_color.set_sensitive(False)
+        color_match_box.pack_start(label_match_color, True, True, 0)
+
+        self.entry_match_color = Gtk.Entry()
+        self.entry_match_color.set_text(
+            self.color_settings['new-matching-color'])
+        color_match_box.pack_start(self.entry_match_color, True, True, 0)
 
         # Base
         base_box = Gtk.HBox(
@@ -191,22 +244,3 @@ class ReplacePage(Gtk.VBox):
         self.button_clear = Gtk.Button(
             image=self.icon_clear, tooltip_text='Clear edits')
         base_box.pack_start(self.button_clear, True, True, 0)
-
-
-class EntryMarkup(Gtk.HBox):
-    """docstring for TemplatesMarkup"""
-    def __init__(self, *args, **kwargs):
-        Gtk.HBox.__init__(self, spacing=6, *args, **kwargs)
-        self.pack_start(Gtk.Label(label='['), True, True, 0)
-
-        self.entry = Gtk.Entry()
-        self.pack_start(self.entry, True, True, 0)
-
-        self.pack_start(Gtk.Label(label=']'), True, True, 0)
-
-    def set_text(self, text: str):
-        self.entry.set_text(text)
-
-        
-
-
